eff7b944011f948a91e7e328cc01fb14ae5a3322
Krivilyov <gramprogramming@gmail.com>
2022-02-08 00:14:55 +0500

    continue war with cors set cookie

--- src/authorisation/authorisation.controller.ts
+++ src/authorisation/authorisation.controller.ts
@@ -27,7 +27,7 @@ export class AuthorisationController {
   ) {
     const token = await this.authorisationServise.login(userDto);
 
-    const cookie = `token=${token}; HttpOnly; Path=/; Max-Age=21600; domain=${process.env.CLIENT_URL} SameSite=None`;
+    const cookie = `token=${token}; HttpOnly; Path=/; Max-Age=21600; Domain=${process.env.CLIENT_URL} SameSite=None`;
     response.setHeader('Set-Cookie', cookie);
     return { success: true };
   }
