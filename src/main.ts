12c920a811f9cb2ba30ba63c3aa5460106c9dfed
Krivilyov <gramprogramming@gmail.com>
2022-02-07 23:34:55 +0500

    credentials: true

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: `${process.env.CLIENT_URL}` });
+  app.enableCors({ origin: true, credentials: true });
   app.use(cookieParser());
   await app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 }
